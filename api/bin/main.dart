0ca1ea348db6d7544ef80340df30a34500b0734d
D10100111001 <D10100111001@users.noreply.github.com>
2020-10-02 20:13:59 -0500

    Update package paths to use github url

--- api/bin/main.dart
+++ api/bin/main.dart
@@ -3,7 +3,8 @@ import 'package:arrahma_web_api/api.dart';
 Future main() async {
   final app = Application<ArrahmaChannel>()
     ..options.configurationFilePath = 'config.yaml'
-    ..options.port = 8888;
+    ..options.port =
+        int.tryParse(Platform.environment['PORT'] ?? 8888.toString());
 
   final count = Platform.numberOfProcessors ~/ 2;
   await app.start(numberOfInstances: count > 0 ? count : 1);
