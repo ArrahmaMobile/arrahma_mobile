f0224c8df409179fce06aae837e4b0b30c595156
D10100111001 <D10100111001@users.noreply.github.com>
2020-10-04 07:49:54 -0500

    Build script for api and git package source

---
+++ api/build.sh
@@ -0,0 +1,13 @@
+cd ../shared
+pub get
+pub run build_runner build --delete-conflicting-outputs
+cd ../scraper
+pub get
+cd ../scraper_service
+pub get
+cd ../api
+pub upgrade
+pub get
+pub upgrade
+pub get
+dart bin/main.dart
