4ac3187a015228505c969c92043b9d98a1611a53
Siddiqui Family <SiddiquiFamily@siddiquis-air.lan>
2020-12-26 23:49:42 -0600

    created an initial Xcode project

---
+++ arrahma/arrahma/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  arrahma
+//
+//  Created by Siddiqui Family on 12/26/20.
+//  Copyright © 2020 Siddiqui Family. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
